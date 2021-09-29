74457a1247a270e5defe6f18501d5c038f38a014
Ganapathi V Rao <76618674+Ganapathivrao@users.noreply.github.com>
2021-09-29 18:58:40 +0530

    First commit

---
+++ new-pro/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+const a="students";
+  return (
+    <>
+        <h1>Welcome to Cognitive Clouds React Training sessions!</h1>
+        <h3>Hello {a}!!</h3>
+    </>
+  );
+}
+
+export default App;
